05fee7f6376f073d4f9ffa6d9b7b3a4e666864cf
nishad <nishad1868@gmail.com>
2024-04-13 10:52:11 +0530

    1

--- array.ts
+++ array.ts
@@ -3,5 +3,5 @@ const a :string[] = ['hi','hello','good']
 console.log(a[0]);
 
 
-const b : [string,boolean,number] = ['hi',true,1]
-console.log(b[1]);
+const ba : [string,boolean,number] = ['hi',true,1]
+console.log(ba[1]);
