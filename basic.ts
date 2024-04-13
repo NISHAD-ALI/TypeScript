a43f4bd3b10b580d5cd31d99e4da1ef3be61f6da
nishad <nishad1868@gmail.com>
2024-04-13 10:54:04 +0530

    2

--- basic.ts
+++ basic.ts
@@ -1,18 +1,18 @@
-// function add(x:number,y:number){
-//     return x+y
-// }
+function add(x:number,y:number){
+    return x+y
+}
 
-// console.log(add(1,2));
+console.log(add(1,2));
 
 
-// function rep(x:string,y:number):string{
-//     let res = ''
-//     for(let i=0;i<x.length;i++){
-//           for(let j=0;j<y;j++){
-//                res += x[i]
-//           }
-//     }
-//     return res
-// }
-// console.log(rep('hello',5));
+function rep(x:string,y:number):string{
+    let res = ''
+    for(let i=0;i<x.length;i++){
+          for(let j=0;j<y;j++){
+               res += x[i]
+          }
+    }
+    return res
+}
+console.log(rep('hello',5));
 
